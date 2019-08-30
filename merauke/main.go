285d4b34e7417005b4081e6116b973181c63a325
Nakama <nakama@200550.local>
2019-08-30 14:56:44 +0700

    temporary save

--- merauke/main.go
+++ merauke/main.go
@@ -1,15 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -18,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -25,13 +14,24 @@ import (
 var bot *linebot.Client
 
 func main() {
+
 	var err error
+
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
+
+	// Line Handler
 	http.HandleFunc("/callback", callbackHandler)
+	http.HandleFunc("/", greet)
+
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
+
 	http.ListenAndServe(addr, nil)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
 func test(ctx string, msg string) {
